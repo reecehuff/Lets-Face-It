b4fccbe45bebdbec5b46ed2cd520f7423736486b
reecehuff <reecedhuff@gmail.com>
2023-03-28 13:39:52 -0700

    Built out verification functionality

--- frames_2_emotions.py
+++ frames_2_emotions.py
@@ -6,6 +6,10 @@ import pandas as pd
 import core.utils as utils
 import core.face as face
 
+#%% Input toggle(s)
+verify          = False # A toggle that if True will verify that the frame contains the chairperson
+all_conferences = True  # A toggle that if True will apply DeepFace to all of the conferences
+
 #%% Define the frames from the videos that will be used to verify the identity of the speaker
 conferences = sorted(glob.glob('data/frames/*'))
 # Define the frames in the video that will be used to identify the chair and save their paths to an excel file
@@ -15,13 +19,22 @@ utils.get_identity_frames(conferences, save_path=identity_frames_excel_path)
 utils.save_identity_frames()
 
 #%% Loop through the conferences and predict the emotions for all of the frames in the videos
-indentity_frames = pd.read_excel(identity_frames_excel_path, index_col=0)
-face.predict_all_emotions(indentity_frames, conferences)
+if all_conferences:
+    indentity_frames = pd.read_excel(identity_frames_excel_path, index_col=0)
+    if verify:
+        face.predict_all_emotions(conferences, indentity_frames)
+    else:
+        face.predict_all_emotions(conferences)
 
 #%% Debug one particular conference if the verification is struggling
 # indentity_frames = pd.read_excel(identity_frames_excel_path, index_col=0)
-# print(conferences)
-# print(conferences[26])
-# indentity_frame_path = indentity_frame_path = utils.get_identity_frame_path(indentity_frames, conferences[26])
-# emotions_df = face.predict_emotions(conferences[26], indentity_frame_path, num_frames=100)
+# conference_of_interest = conferences[0]
+# num_frames = None
+# print(conference_of_interest)
+# if verify:
+#     indentity_frame_path = utils.get_identity_frame_path(conference_of_interest, indentity_frames)
+#     emotions_df = face.predict_emotions(conference_of_interest, indentity_frame_path, num_frames=num_frames)
+# else:
+#     emotions_df = face.predict_emotions(conference_of_interest, num_frames=num_frames)
+
 # print(emotions_df)
