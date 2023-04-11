b75f4cb330096e75387a8de4e040e6359b555f24
reecehuff <reecedhuff@gmail.com>
2023-04-11 14:11:43 -0700

    Swapped ind and dep vars; Added trump YT videos

--- emotions_vs_returns.py
+++ emotions_vs_returns.py
@@ -4,39 +4,55 @@ import pandas as pd
 #--Scripts
 from core.plotter import Plotter
 
-#%% Initialize the Plotter class
-plotter = Plotter()
-
 #%% Read in the final csv file of the mean_neg_emotion, std_neg_emotion, pca_emotion, and dmd_neg_emotion 
-df = pd.read_csv('data/emotions_vs_returns/returns_volumes_negative_emotions.csv', index_col=0)
-print(df)
+
+# You need to run process_emotions_and_returns.py with the same which_youtube_videos before running this script
+which_youtube_videos = 'trump'
+
+if which_youtube_videos == 'trump':
+    #---Final path
+    final_path = 'data/emotions_vs_returns/trump/returns_volumes_negative_emotions.csv'
+    figures_path = "figures/trump"
+
+elif which_youtube_videos == 'FOMC':
+    #---Final path
+    final_path = 'data/emotions_vs_returns/FOMC/returns_volumes_negative_emotions.csv'
+    figures_path = "figures/FOMC"
+
+else:
+    raise "%s is a not a valid input to which_youtube_ids" % which_youtube_videos
+
+df = pd.read_csv(final_path, index_col=0)
+
+#%% Initialize the Plotter class
+plotter = Plotter(figures_dir=figures_path)
 
 #%% Plot the returns, volumes, and negative emotions
 
 #-----------RETURNS-----------#
 #---Returns vs. mean negative emotion
-plotter.default_negative_emotions('return', '', 'upper left')
-plotter.negative_emotions(df,'return', 'mean_neg_emotion', 'returns_vs_mean_neg_emotion.png')
+plotter.default_negative_emotions('', 'return', 'upper left')
+plotter.negative_emotions(df, 'mean_neg_emotion', 'return', 'returns_vs_mean_neg_emotion.png')
 #---Returns vs. std_neg_emotion
-plotter.default_negative_emotions('return', 'std', 'lower left')
-plotter.negative_emotions(df,'return', 'std_neg_emotion', 'returns_vs_std_neg_emotion.png')
+plotter.default_negative_emotions('std', 'return', 'lower left')
+plotter.negative_emotions(df, 'std_neg_emotion', 'return', 'returns_vs_std_neg_emotion.png')
 #---Returns vs. pca_emotion
-plotter.default_negative_emotions('return', 'pca', 'upper left')
-plotter.negative_emotions(df,'return', 'pca_emotion', 'returns_vs_pca_emotion.png')
+plotter.default_negative_emotions('pca', 'return', 'upper left')
+plotter.negative_emotions(df, 'pca_emotion', 'return', 'returns_vs_pca_emotion.png')
 #---Returns vs. dmd_neg_emotion
-plotter.default_negative_emotions('return', 'dmd', 'upper left')
-plotter.negative_emotions(df,'return', 'dmd_neg_emotion', 'returns_vs_dmd_neg_emotion.png')
+plotter.default_negative_emotions('dmd', 'return', 'upper left')
+plotter.negative_emotions(df, 'dmd_neg_emotion', 'return', 'returns_vs_dmd_neg_emotion.png')
 
 #-----------VOLUMES-----------#
 #---Returns vs. mean negative emotion
-plotter.default_negative_emotions('volume', '', 'upper right')
-plotter.negative_emotions(df,'volume', 'mean_neg_emotion', 'volumes_vs_mean_neg_emotion.png')
+plotter.default_negative_emotions('', 'volume', 'upper right')
+plotter.negative_emotions(df, 'mean_neg_emotion', 'volume', 'volumes_vs_mean_neg_emotion.png')
 #---volumes vs. std_neg_emotion
-plotter.default_negative_emotions('volume', 'std', 'lower right')
-plotter.negative_emotions(df,'volume', 'std_neg_emotion', 'volumes_vs_std_neg_emotion.png')
+plotter.default_negative_emotions('std', 'volume', 'lower right')
+plotter.negative_emotions(df, 'std_neg_emotion', 'volume', 'volumes_vs_std_neg_emotion.png')
 #---volumes vs. pca_emotion
-plotter.default_negative_emotions('volume', 'pca', 'upper right')
-plotter.negative_emotions(df,'volume', 'pca_emotion', 'volumes_vs_pca_emotion.png')
+plotter.default_negative_emotions('pca', 'volume', 'upper right')
+plotter.negative_emotions(df, 'pca_emotion', 'volume', 'volumes_vs_pca_emotion.png')
 #---volumes vs. dmd_neg_emotion
-plotter.default_negative_emotions('volume', 'dmd', 'upper right')
-plotter.negative_emotions(df,'volume', 'dmd_neg_emotion', 'volumes_vs_dmd_neg_emotion.png')
+plotter.default_negative_emotions('dmd', 'volume', 'upper right')
+plotter.negative_emotions(df, 'dmd_neg_emotion', 'volume', 'volumes_vs_dmd_neg_emotion.png')
